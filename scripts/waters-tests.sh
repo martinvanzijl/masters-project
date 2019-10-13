bb5b5a8be3f2ced8b23048aef3a4b35fac335f46
Martin van Zijl <martin.vanzijl@gmail.com>
2019-10-14 11:49:44 +1300

    Test from CSV input file.

--- scripts/waters-tests.sh
+++ scripts/waters-tests.sh
@@ -1,12 +1,12 @@
 # Run WATERS tests for my thesis.
 
 # Constants.
-#MODEL="../models/model-2-01-nginx.wmod"
-MODEL="../models/model-2-02-nodejs.wmod"
-OUTPUT_FILE="../results/waters-results.txt"
+MODEL="/home/martin/Desktop/github/models/model-2-01-nginx.wmod"
+#MODEL="../models/model-2-02-nodejs.wmod"
+OUTPUT_FILE="/home/martin/Desktop/github/results/waters-results.txt"
 
 # Change to "wcheck" directory.
-pushd ../waters
+pushd ~/Desktop/waters
 
 # Back up output file.
 if [ -f $OUTPUT_FILE ]
@@ -15,62 +15,46 @@ then
 fi
 
 # Run tests.
-#for ((max_rps = 100; max_rps <= 1000; max_rps += 100))
+#for ((scale_cpu = 50; scale_cpu <= 100; scale_cpu += 50))
 #do
-    #echo "Testing max rps $max_rps"
-    #echo "Test with MAX_REQUESTS_PER_SECOND = $max_rps" >> $OUTPUT_FILE
-    #./wcheck -bdd -lang -q -DMAX_REQUESTS_PER_SECOND=$max_rps ../models/model-2-01-nginx.wmod >> $OUTPUT_FILE
+#    for ((pod_max = 1; pod_max <= 4; pod_max += 1))
+#	do
+#		for ((max_rps = 1; max_rps <= 2; max_rps += 1))
+#		do
+#			echo "Testing max_rps=$max_rps, pod_max=$pod_max, scale_cpu=$scale_cpu..."
+#			./wcheck -bdd -lang -q -DMAX_REQUESTS_PER_SECOND=$max_rps -DPOD_MAX=$pod_max -DSCALE_CPU_THRESHOLD=$scale_cpu $MODEL >> $OUTPUT_FILE
+#		done
+#	done
 #done
 
-#for ((pods_initial = 1; pods_initial <= 4; pods_initial += 1))
-#do
-    #echo "Testing pods_initial $pods_initial"
-    #echo "Test with PODS_INITIALLY_ON = $pods_initial" >> $OUTPUT_FILE
-    #./wcheck -bdd -lang -q -DPODS_INITIALLY_ON=$pods_initial ../models/model-2-01-nginx.wmod >> $OUTPUT_FILE
-#done
+# Test from CSV file.
+INPUT_FILE="/home/martin/Desktop/github/results/test-cases.csv"
+HEADER_READ=0
 
-#for ((pod_max = 3; pod_max <= 6; pod_max += 1))
-#do
-    #echo "Testing pod_max $pod_max"
-    #echo "Test with POD_MAX = $pod_max" >> $OUTPUT_FILE
-    #./wcheck -bdd -lang -q -DPOD_MAX=$pod_max ../models/model-2-01-nginx.wmod >> $OUTPUT_FILE
-#done
+processing_time=6
 
-#for ((scale_cpu = 10; scale_cpu <= 100; scale_cpu += 10))
-#do
-    #echo "Testing scale_cpu $scale_cpu"
-    #echo "Test with SCALE_CPU_THRESHOLD = $scale_cpu" >> $OUTPUT_FILE
-    #./wcheck -bdd -lang -q -DSCALE_CPU_THRESHOLD=$scale_cpu ../models/model-2-01-nginx.wmod >> $OUTPUT_FILE
-#done
-
-#for ((max_rps = 200; max_rps <= 400; max_rps += 100))
-#do
-	#for ((pods_initial = 1; pods_initial <= 4; pods_initial += 1))
-	#do
-		#echo "Testing max_rps $max_rps pods_initial $pods_initial"
-		#echo "Test with MAX_REQUESTS_PER_SECOND $max_rps PODS_INITIALLY_ON = $pods_initial" >> $OUTPUT_FILE
-		#./wcheck -bdd -lang -q -DMAX_REQUESTS_PER_SECOND=$max_rps -DPODS_INITIALLY_ON=$pods_initial ../models/model-2-01-nginx.wmod >> $OUTPUT_FILE
-	#done
-#done
-
-#for ((pod_max = 1; pod_max <= 4; pod_max += 1))
-for ((scale_cpu = 50; scale_cpu <= 100; scale_cpu += 50))
+while IFS=, read -r max_rps pod_min pod_max initial_pods scale_cpu
 do
-	#for ((scale_cpu = 50; scale_cpu <= 100; scale_cpu += 50))
-    for ((pod_max = 1; pod_max <= 4; pod_max += 1))
-	do
-		for ((max_rps = 1; max_rps <= 2; max_rps += 1))
-		do
-			echo "Testing max_rps=$max_rps, pod_max=$pod_max, scale_cpu=$scale_cpu..."
-			./wcheck -bdd -lang -q -DMAX_REQUESTS_PER_SECOND=$max_rps -DPOD_MAX=$pod_max -DSCALE_CPU_THRESHOLD=$scale_cpu $MODEL >> $OUTPUT_FILE
-		done
-	done
-done
+    if ((HEADER_READ==0))
+    then
+        HEADER_READ=1
+    else
+        echo "Testing: $max_rps|$pod_min|$pod_max|$initial_pods|$scale_cpu"
+        ./wcheck -bdd -lang -q  -DMAX_REQUESTS_PER_SECOND=$max_rps \
+                                -DPOD_MIN=$pod_min \
+                                -DPOD_MAX=$pod_max \
+                                -DPROCESSING_TIME_PER_REQ_IN_MS=$processing_time \
+                                -DSCALE_CPU_THRESHOLD=$scale_cpu \
+                                $MODEL \
+                                 >> $OUTPUT_FILE
+    fi
+done < $INPUT_FILE
 
 # Go back to the previous directory.
 popd
 
 # Analyse the results
-./analyse-waters-results.awk $OUTPUT_FILE > ../results/waters-analysis.csv
-echo "Results are in ../results/waters-analysis.csv"
+ANALYSIS_FILE="/home/martin/Desktop/github/results/waters-analysis.csv"
+./analyse-waters-results.awk $OUTPUT_FILE > $ANALYSIS_FILE
+echo "Results are in $ANALYSIS_FILE"
 
