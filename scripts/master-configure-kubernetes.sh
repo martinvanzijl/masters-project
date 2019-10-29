5e368dcde7483f12ac8195c989a1cc897133dc19
Martin van Zijl <martin.vanzijl@gmail.com>
2019-10-30 09:54:21 +1300

    No need to kill pods.

--- scripts/master-configure-kubernetes.sh
+++ scripts/master-configure-kubernetes.sh
@@ -8,8 +8,8 @@ starting_pods=${4:-2}
 app=${5:-bad-app} # Stop if an invalid app is given.
 
 # First kill the existing pods.
-NAMESPACE=$app-namespace
-kubectl delete pod --namespace=$NAMESPACE --all
+#NAMESPACE=$app-namespace
+#kubectl delete pod --namespace=$NAMESPACE --all
 
 # Apply parameters to Kubernetes.
 cat $app.template.yml | sed "s/{{scale_cpu_threshold}}/$scale_cpu_threshold/g;s/{{min_pods}}/$min_pods/g;s/{{max_pods}}/$max_pods/g;s/{{starting_pods}}/$starting_pods/g" | kubectl apply -f - --namespace=$app-namespace
