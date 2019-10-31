590043541d97ad990329777f11d440b443cfbc36
Martin van Zijl <martin.vanzijl@gmail.com>
2019-11-01 09:17:31 +1300

    Tested varying the initial pods paramter.

--- scripts/jmeter-tests-with-shaping-timer.sh
+++ scripts/jmeter-tests-with-shaping-timer.sh
@@ -9,15 +9,11 @@ MINUTES_PER_TRIAL=2
 MAX_ERROR_RATE_PERCENT=1
 MAX_RESPONSE_TIME_IN_MS=10000
 
-# Cluster
-MIN_REPLICAS=1
-STARTING_REPLICAS=1
-
 # Request details.
-REQUEST_PATH="/api/v1/namespaces/nginx-namespace/services/nginx:80/proxy/"
-### REQUEST_PATH="/api/v1/namespaces/nodejs-namespace/services/nodejs-service:8080/proxy/"
-APP="nginx"
-### APP="nodejs"
+#REQUEST_PATH="/api/v1/namespaces/nginx-namespace/services/nginx:80/proxy/"
+REQUEST_PATH="/api/v1/namespaces/nodejs-namespace/services/nodejs-service:8080/proxy/"
+#APP="nginx"
+APP="nodejs"
 
 # Files.
 OVERALL_RESULTS_FILE="/home/mv22/Desktop/results/overall-results.csv"
@@ -28,19 +24,25 @@ CPU_SCALE_THRESHOLD_MIN=80
 CPU_SCALE_THRESHOLD_MAX=80
 CPU_SCALE_THRESHOLD_INC=1
 
-MAX_RPS_MIN=100
-MAX_RPS_MAX=400
-MAX_RPS_INC=100
+MIN_REPLICAS=1
 
-MAX_REPLICAS_MIN=1
+MAX_RPS_MIN=1
+MAX_RPS_MAX=5
+MAX_RPS_INC=1
+
+MAX_REPLICAS_MIN=4
 MAX_REPLICAS_MAX=4
 MAX_REPLICAS_INC=1
+
+STARTING_REPLICAS_MIN=1
+STARTING_REPLICAS_MAX=4
+STARTING_REPLICAS_INC=1
 
 # Calculate duration.
 duration_in_seconds=$(($MINUTES_PER_TRIAL*60))
 
 # Calculate total test cases.
-TOTAL_TEST_CASES=$(( ((CPU_SCALE_THRESHOLD_MAX-CPU_SCALE_THRESHOLD_MIN)/CPU_SCALE_THRESHOLD_INC + 1) * ((MAX_RPS_MAX-MAX_RPS_MIN)/MAX_RPS_INC + 1) * ((MAX_REPLICAS_MAX-MAX_REPLICAS_MIN)/MAX_REPLICAS_INC + 1) ))
+TOTAL_TEST_CASES=$(( ((CPU_SCALE_THRESHOLD_MAX-CPU_SCALE_THRESHOLD_MIN)/CPU_SCALE_THRESHOLD_INC + 1) * ((MAX_RPS_MAX-MAX_RPS_MIN)/MAX_RPS_INC + 1) * ((MAX_REPLICAS_MAX-MAX_REPLICAS_MIN)/MAX_REPLICAS_INC + 1) * ((STARTING_REPLICAS_MAX-STARTING_REPLICAS_MIN)/STARTING_REPLICAS_INC + 1) ))
 MINUTES_PER_TEST_CASE=$((TRIALS_PER_TEST_CASE*MINUTES_PER_TRIAL))
 test_case_number=0
 
@@ -54,42 +56,35 @@ echo "Duration (s),Start,End,Req. per Sec. (low),Req. per Sec. (high),High Durat
 pushd ~/Desktop/apache-jmeter-5.1.1
 
 # Loop through values for users.
-#for((users=5;users<=5;users+=1))
-#do
+for((starting_replicas=$STARTING_REPLICAS_MIN;starting_replicas<=$STARTING_REPLICAS_MAX;starting_replicas+=$STARTING_REPLICAS_INC))
+do
     # Loop through values for maximum replicas.
     for((max_replicas=$MAX_REPLICAS_MIN;max_replicas<=$MAX_REPLICAS_MAX;max_replicas+=$MAX_REPLICAS_INC))
     do
-        # Ensure starting replicas is sensible.
-        if(($STARTING_REPLICAS>$max_replicas))
-        then
-            echo "Starting replicas is too high, setting to max_replicas which is $max_replicas."
-            STARTING_REPLICAS=$max_replicas
-        fi
-
 	    # Loop through values for CPU scaling threshold.
 	    for((cpu_scale_threshold=$CPU_SCALE_THRESHOLD_MIN;cpu_scale_threshold<=$CPU_SCALE_THRESHOLD_MAX;cpu_scale_threshold+=$CPU_SCALE_THRESHOLD_INC))
 	    do
 		    # Configure the cluster.
-		    ssh donner "./server-configure-kubernetes.sh $cpu_scale_threshold $MIN_REPLICAS $max_replicas $STARTING_REPLICAS $APP"
+		    ssh donner "./server-configure-kubernetes.sh $cpu_scale_threshold $MIN_REPLICAS $max_replicas $starting_replicas $APP"
 
 		    # Loop through values for requests per second.
 		    for((max_rps=$MAX_RPS_MIN;max_rps<=$MAX_RPS_MAX;max_rps+=$MAX_RPS_INC))
 		    do
 			    # Print information.
 			    test_case_number=$((test_case_number+1))
-			    echo "Running test case #$test_case_number/$TOTAL_TEST_CASES: cpu=$cpu_scale_threshold, max_replicas=$max_replicas, rps(high load)=$max_rps..."
+			    echo "Running test case #$test_case_number/$TOTAL_TEST_CASES: cpu=$cpu_scale_threshold, starting_replicas=$starting_replicas, max_replicas=$max_replicas, rps(high load)=$max_rps..."
 
 			    # Estimate time left.
-			    ### awk "BEGIN {printf \"About %.1f minutes to go...\n\", ($TOTAL_TEST_CASES - $test_case_number + 1)*$MINUTES_PER_TEST_CASE}"
+			    awk "BEGIN {printf \"About %.1f minutes to go...\n\", ($TOTAL_TEST_CASES - $test_case_number + 1)*$MINUTES_PER_TEST_CASE}"
 
 			    # Calculate maximum requests per minutes.
-			    ### max_rpm=$(($max_rps*60))
+			    max_rpm=$(($max_rps*60))
 
 			    # Run the trials for a single test.
 			    for((trial_number=1;trial_number<=TRIALS_PER_TEST_CASE;trial_number++))
 			    do
 				    # Pad with zeroes.
-				    printf -v trial_string "%02d_%02d_%03d_%03d_%02d" $users $max_replicas $cpu_scale_threshold $max_rps $trial_number
+				    printf -v trial_string "%02d_%02d_%02d_%03d_%03d_%02d" $users $starting_replicas $max_replicas $cpu_scale_threshold $max_rps $trial_number
 
 				    # Calculate results file.
 				    JMETER_OUT_FILE="/home/mv22/Desktop/results/jmeter-$trial_string.txt"
@@ -100,8 +95,11 @@ pushd ~/Desktop/apache-jmeter-5.1.1
 				    echo "Running trial number #$trial_number/$TRIALS_PER_TEST_CASE..."
 
 				    # Wait for cluster to be ready.
+                    echo "Killing existing pods..."
+                    ssh donner "./server-kill-pods.sh $APP"
+                    echo "Pods killed..."
 				    echo "Waiting for cluster to be ready..."
-				    ssh donner "./server-wait-till-cluster-ready.sh $STARTING_REPLICAS $APP"
+				    ssh donner "./server-wait-till-cluster-ready.sh $starting_replicas $APP"
 				    echo "Cluster is ready..."
 
 				    # Calculate start time.
@@ -121,14 +119,6 @@ pushd ~/Desktop/apache-jmeter-5.1.1
                     -Jmax_rps_high=$max_rps_high #\
                     #> $JMETER_OUT_FILE
 
-				    ### jmeter -n -t ~/Desktop/github/jmeter/test-plan.jmx \
-                    ### -Joutput_csv_file="$RESULTS_FILE" \
-                    ### -Joutput_report_file="$JMETER_REPORT_FILE" \
-                    ### -Jduration_in_seconds=$duration_in_seconds \
-                    ### -Jmax_rpm=$max_rpm \
-                    ### -Jrequest_path=$REQUEST_PATH \
-                    ### -Jusers=$users \
-
                     # Create summary report file.
                     ./bin/JMeterPluginsCMD.sh --generate-csv $JMETER_REPORT_FILE --input-jtl $JTL_FILE --plugin-type AggregateReport
 
@@ -140,7 +130,7 @@ pushd ~/Desktop/apache-jmeter-5.1.1
                     low_duration=60
                     high_duration=60
                     max_error_rate=1
-				    printf "$duration_in_seconds,$trial_start_time,$trial_end_time,$max_rps_low,$max_rps_high,$high_duration,$low_duration,$app_response_time,$MIN_REPLICAS,$max_replicas,$STARTING_REPLICAS,$cpu_scale_threshold,$MAX_RESPONSE_TIME_IN_MS,$max_error_rate," >> $OVERALL_RESULTS_FILE
+				    printf "$duration_in_seconds,$trial_start_time,$trial_end_time,$max_rps_low,$max_rps_high,$high_duration,$low_duration,$app_response_time,$MIN_REPLICAS,$max_replicas,$starting_replicas,$cpu_scale_threshold,$MAX_RESPONSE_TIME_IN_MS,$max_error_rate," >> $OVERALL_RESULTS_FILE
 
                     # Analyse with Python script.
 				    $PYTHON_SCRIPT $JMETER_REPORT_FILE $OVERALL_RESULTS_FILE
@@ -148,7 +138,7 @@ pushd ~/Desktop/apache-jmeter-5.1.1
 		    done
 	    done
     done
-#done
+done
 
 # Back up.
 BACKUP_FOLDER="/home/mv22/Desktop/results/backup-"`date +"%Y-%m-%d-%H%M"`
