7db984bca3d477149b8487768022a324a45ddb6a
Martin van Zijl <martin.vanzijl@gmail.com>
2019-12-05 10:19:24 +1300

    Make backups.

--- scripts/waters-tests.sh
+++ scripts/waters-tests.sh
@@ -1,6 +1,7 @@
 # Run WATERS tests for my thesis.
 
 # Constants.
+#MODEL=~/Desktop/github/models/model-2-01Z-nginx-final.wmod
 MODEL=~/Desktop/github/models/model-2-02Z-nodejs-final.wmod
 OUTPUT_FILE=~/Desktop/github/results/waters-results.txt
 
@@ -32,20 +33,26 @@ fi
 #done
 
 # Test from CSV file.
-#INPUT_FILE=~/Desktop/github/results/test-cases.csv
+INPUT_FILE=~/Desktop/github/results/test-cases.csv
 #INPUT_FILE=~/Desktop/github/results/speed-test-cases.csv
-INPUT_FILE=~/Desktop/github/results/waters-consistency-test-cases.csv
+#INPUT_FILE=~/Desktop/github/results/waters-consistency-test-cases.csv
 HEADER_READ=0
 
-while IFS=, read -r rps_low rps_high high_duration low_duration app_processing_time min_pods max_pods initial_pods scale_cpu
+#NGINX
 #while IFS=, read -r rps min_pods max_pods initial_pods scale_cpu
+# Node.js
+while IFS=, read -r rps_low rps_high high_duration low_duration app_processing_time min_pods max_pods initial_pods scale_cpu
 do
     if ((HEADER_READ==0))
     then
         HEADER_READ=1
     else
-        echo "Testing: $rps_low|$rps_high|$high_duration|$low_duration|$app_processing_time|$min_pods|$max_pods|$initial_pods|$scale_cpu"
+        # NGINX
         #echo "Testing: $rps|$min_pods|$max_pods|$initial_pods|$scale_cpu"
+        # Node.js
+        echo "Testing: $rps_low|$rps_high|$high_duration|$low_duration|$app_processing_time|$min_pods|$max_pods|$initial_pods|$scale_cpu"
+
+        # NGINX
         #app_processing_time=6 # hard code response time
 
         # NGINX
@@ -84,4 +91,13 @@ popd
 ANALYSIS_FILE=~/Desktop/github/results/waters-analysis.csv
 ./analyse-waters-results.awk $OUTPUT_FILE > $ANALYSIS_FILE
 echo "Results are in $ANALYSIS_FILE"
+
+# Back up.
+BACKUP_FOLDER=~/Desktop/github/results/waters/backup-`date +"%Y-%m-%d-%H%M"`
+mkdir $BACKUP_FOLDER
+cp $MODEL $BACKUP_FOLDER
+cp $OUTPUT_FILE $BACKUP_FOLDER
+cp $INPUT_FILE $BACKUP_FOLDER
+cp $ANALYSIS_FILE $BACKUP_FOLDER
+cp $0 $BACKUP_FOLDER
 
