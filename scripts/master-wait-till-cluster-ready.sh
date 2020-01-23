98501dbc7b7cf7d0611df1504ee081ffa4dad737
Martin van Zijl <martin.vanzijl@gmail.com>
2020-01-24 12:16:54 +1300

    Added comment about potential flaw.

--- scripts/master-wait-till-cluster-ready.sh
+++ scripts/master-wait-till-cluster-ready.sh
@@ -7,6 +7,26 @@
 APP=$2
 NAMESPACE=$APP-namespace
 
+# There is a flaw here. Pods in "ContainerCreating" status may mean the actual
+# number of pods running at the start of the trial may be greater than the
+# the "starting pods" parameter in the YAML configuration file.
+# (Discovered on 24 Jan 2020.)
+
+# This is important since the starting number of pods has great influence on
+# SLA compliance.
+
+# From a research point of view, this means that the "Initial Pods" parameter
+# in my results does not truly reflect the starting number of pods in the trial.
+# This is really a dependent variable that based partly on the "leftover" metrics
+# from the previous trial (remember that the Kubernetes HPA discards metrics from
+# pods under a certain age).
+
+# This could be fixed by checking that "runnings==pods" instead of "running>=pods"
+# and also checking that container_creating==0. This may add a substantial delay
+# between the running of trials, however. This could also possibly be fixed by adding a
+# fixed delay (about 5 minutes) after re-configuring the cluster, before
+# commencing the JMeter load test.
+
 # Wait for the cluster to set up again.
 result=`kubectl get pods --namespace=$NAMESPACE | awk -v pods=$1 '
         /Pending/{pending++}
